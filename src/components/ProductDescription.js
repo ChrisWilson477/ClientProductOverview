cb34b06d9cf28abb62370a201d5fe99cebbbbda6
Chris <68999991+ChrisWilson477@users.noreply.github.com>
2021-03-31 10:16:46 -0600

    CSS updat price

--- src/components/ProductDescription.js
+++ src/components/ProductDescription.js
@@ -10,7 +10,8 @@ const ProductDescription = () => {
 		<Col sm={4} className='right-col'>
       <h3>{Mock[0].name}{' -'}{Mock[0].category}</h3>
       <Ratings />
-      <p className='overview-price'><span style={{textDecoration: 'line-through', paddingRight: '20px' }}>{'$'}{Mock[0].default_price}{'.00'}</span>{'$'}{Mock[1].results[0].sale_price}{'.00'}</p>
+      <p className='overview-price'>{'$'}{Mock[1].results[0].sale_price}{'.00'}<span style={{textDecoration: 'line-through', paddingLeft: '20px', color:'#000' }}>{'$'}{Mock[0].default_price}{'.00'}</span></p>
+      <p className='reduced'>REDUCED PRICE!</p>
       <p>{Mock[0].description}</p>
 
       <button type='button' className='button'>-</button>
